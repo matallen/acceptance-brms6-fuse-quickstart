ad489355d7f4af9514e65b408765e98df3e22b8a
Mat Allen <mallen@mallen.redhat>
2015-03-26 09:00:40 -0500

    updated repository location

--- tools/init.sh
+++ tools/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git clone https://github.com/matallen/camel-kie-example
+git clone https://github.com/matallen/camel-kie
 git clone https://github.com/matallen/maven-fusecontainer-plugin
